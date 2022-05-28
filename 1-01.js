6c565e41503c9361d2a0d8de5c34249070ec5870
yoshuu <appcs342@gmail.com>
2022-05-28 11:29:34 +0800

    feat:1-01ok

--- 1-01.js
+++ 1-01.js
@@ -1,1 +1,15 @@
-apple;
+// 我的寫法
+
+// for
+
+// for (var i = 1; i < 10; i++) {
+//   console.log(i);
+// }
+
+// while
+
+// var i = 0;
+// while (i < 9) {
+//   i = i + 1;
+//   console.log(i);
+// }
